93022083dfdaa86bd0a1ebfcab8bbb655db1c72e
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-08-01 22:34:07 +0500

    Import fonts from google.

--- Week1/Day5/tailwind.config.js
+++ Week1/Day5/tailwind.config.js
@@ -1,12 +1,14 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./index.html"],
-   theme: {
+    content: ["./index.html"],
+    theme: {
         extend: {
             fontFamily: {
-                poppins: ['"Poppins"', 'sans-serif'],
-            }
+                poppins: ['Poppins', 'sans-serif'],
+                spacemono: ['"Space Mono"', 'monospace'],
+                worksans: ['"Work Sans"', 'sans-serif'],
+            },
         },
     },
-   plugins: [],
- }
+    plugins: [],
+}
