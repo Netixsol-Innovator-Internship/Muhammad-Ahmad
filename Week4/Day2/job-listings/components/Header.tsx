c289643da1f5df50d9b86e1dbb5362c381b9e84a
m-ahmad-intern <muhammad_ahmad_intern@netixsol.com>
2025-08-19 16:15:59 +0500

    Add header component.

---
+++ Week4/Day2/job-listings/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div
+            className="bg-[url(/images/bg-header-desktop.svg)] bg-[#5da5a4] w-full h-[80px] bg-cover bg-center"></div>
+    )
+}
+
+export default Header
